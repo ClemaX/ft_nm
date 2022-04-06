835e363a53926d3206a7e32211f652a633d26642
Clément Hamada <clementhamada@protonmail.com>
2022-04-07 00:10:55 +0200

    Update nm_tester

--- nm_tester.sh
+++ nm_tester.sh
@@ -1,18 +1,40 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 trap '[ -z "$TMP_DIR" ] || rm -rf "$TMP_DIR"' EXIT
 
-FT_NM=./ft_nm
+FT_NM="$(dirname "$0")/ft_nm"
 BIN_DIR=/bin
 
-# Sort using lexicographical comparison 
-export LC_COLLATE=C
-
+# Create temporary directory for fake path
 export TMP_DIR=$(mktemp -d)
 
+# Compile ft_nm if it does not exist
+[ -f "$FT_NM" ] || make -C "$(dirname "$FT_NM")" NAME="$(basename "$FT_NM")"
+
+# Link ft_nm to fake nm in temporary directory
 ln -s "$PWD/$FT_NM" "$TMP_DIR/nm"
+
+# Set diff colors
+[ -t 1 ] && DIFF_COLOR=always || DIFF_COLOR=never
+
+# Compare nm outputs and status codes
+nm_diff() # arguments
+{
+	local GREP=$(which grep)
+
+	diff "--color=$DIFF_COLOR" \
+		<(set -o pipefail; LC_COLLATE=C \
+			nm "$1" 2>&1 | "$GREP" -v "bfd plugin" \
+			|| echo "status: $?") \
+		<(set -o pipefail; PATH="$TMP_DIR"; LC_COLLATE=C \
+			nm "$1" 2>&1 | "$GREP" -v "bfd plugin" \
+			|| echo "status: $?")
+}
+
+truncate --size 0 errors.log
 
 for binary in $(find "$BIN_DIR/")
 do
-	diff <(nm "$binary" 2>&1 || echo "status: $?") <(PATH="$TMP_DIR"; nm "$binary" 2>&1 || echo "status: $?") || echo "with '$binary'"
+	DIFF=$(nm_diff "$binary") \
+	|| (printf '%-24s:\n%s\n' "$binary" "$DIFF" && echo "$binary" >> errors.log)
 done
